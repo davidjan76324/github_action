b7001d184600f7c94e55980eef4cd0a3d3fe78e6
davidjan76324 <david_jan76324@hotmail.com>
2022-05-31 12:43:00 +0800

    @change url

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import os
 import re
 from bs4 import BeautifulSoup
 
-hotpage = requests.get("https://www.ptt.cc/bbs/hotboards.html")
+hotpage = requests.get("https://tw.yahoo.com/")
 main = BeautifulSoup(hotpage.text, 'html.parser')
 print(main.text) #這裡可以print看看已抓取到除標籤外的文字
