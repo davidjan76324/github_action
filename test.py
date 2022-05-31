458cb2d01afbba68d5fcc6df0e39906c67c40b1d
davidjan76324 <david_jan76324@hotmail.com>
2022-05-31 12:36:51 +0800

    @add requitement.txt and test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello World!!")
+import requests
+import json
+import os
+import re
+from bs4 import BeautifulSoup
+
+hotpage = requests.get("https://www.ptt.cc/bbs/hotboards.html")
+main = BeautifulSoup(hotpage.text, 'html.parser')
+print(main.text) #這裡可以print看看已抓取到除標籤外的文字
